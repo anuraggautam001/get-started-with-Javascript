31c363eee1fb13c96f2a4580e7d3baca4a76b2fe
WELCOME <anuraggautam876@gmail.com>
2021-11-10 14:53:47 +0530

    learning Javascript

---
+++ first.js
@@ -0,0 +1,18 @@
+var firstName = "john";
+console.log(firstName);
+
+
+var lastName = "smith";
+var age = 28;  
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = "developer";
+console.log(job);
+
+
+   
---
+++ task2.js
@@ -0,0 +1,3 @@
+let name= prompt("What is your name?");
+confirm("your name is "+name);
+alert("fuck you "+name);
