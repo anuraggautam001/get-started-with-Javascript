cb01b85f0057e021e09d879f947ec2c7d7673522
Anurag <anuraggautam876@gmail.com>
2022-02-07 14:22:14 +0530

    Functions in javascript

---
+++ F2.js
@@ -0,0 +1,16 @@
+var pi=3.14;
+
+function calculateArea(radius){
+    alert('Area of circle is '+(pi*radius*radius))
+}
+
+function addoncondition(x,y,z){
+    if(z==1){
+        res=add(x,y);
+    }
+}
+
+function add(x,y){
+    r=x+y;
+    alert("addition is "+r);
+}
