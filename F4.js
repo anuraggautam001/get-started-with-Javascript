30553560f987fbff8708e348ca98f8eb7e421d48
Anurag <anuraggautam876@gmail.com>
2022-02-08 10:16:51 +0530

    Pop-up Box

---
+++ F4.js
@@ -0,0 +1,13 @@
+function hello(){
+    return confirm("Are you sure you want to submit the form");
+}
+
+function add(){
+    var x=prompt("Enter first number");
+    var y=prompt("Enter second number");
+    alert("The Sum is: "+(parseInt(x)+parseInt(y)));
+}
+
+function al(){
+    alert("Hello!! I am Alert Box");
+}
